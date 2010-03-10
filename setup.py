da8e1215b7a57b9691da24b686275169bb0e156d
winterTTr <winterTTr@localhost>
2010-03-10 14:22:39 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='pyvim',
+      version='1.0',
+      description= 'Python package for programming function in vim internal enviroment.',
+      author='winterTTr',
+      author_email='winterTTr@gmail.com',
+      url='http://code.google.com/p/pyvim',
+      license = 'LGPL' ,
+      packages=['pyvim'],
+      package_dir = { 'pyvim' : 'src' } ,
+      )
